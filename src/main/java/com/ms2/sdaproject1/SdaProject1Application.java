27da7d424191396b54fa2f9b1902e0d0f886dfa1
Michal <mike3129@gmail.com>
2020-10-03 10:09:10 +0200

    My first Spring project

---
+++ src/main/java/com/ms2/sdaproject1/SdaProject1Application.java
@@ -0,0 +1,13 @@
+package com.ms2.sdaproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdaProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms2/sdaproject1/SdaProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms2.sdaproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
